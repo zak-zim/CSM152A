a0289a7d65f7b53aa6034e4d52a4168385f1a566
Marc Luzuriaga <Mjluzuriaga109@gmail.com>
2024-05-22 15:15:02 -0700

    Modified StopTop

--- lab3/StopTop.v
+++ lab3/StopTop.v
@@ -1,20 +1,24 @@
-//module StopTop(reset, clk, sel, adj, pause);
+module StopTop(clk, Anode_select, LED_value);
 
 //input reset;
-//input clk;
+input clk;
 //input sel;
 //input adj;
 //input pause;
 
-//reg counterClk;
-//reg adjClk;
-//reg dispClk;
-//reg blinkClk;
+    output [3:0] Anode_select;
+    output [6:0] LED_value;
+
+wire counterClk;
+wire adjClk;
+wire dispClk;
+wire blinkClk;
 //wire [2:0] enables;
 //wire [5:0] min;
 //wire [5:0] sec;
 
-//clock clock(clk, counterClk, adjClk, dispClk, blinkClk);
-//StopController controller(clk, adj, pause, enables);
+clock clock(clk, counterClk, adjClk, dispClk, blinkClk);
+seven_seg_display ssd(1,1,dispClk,Anode_select,LED_value);
 
-//endmodule
+//StopController controller(clk, adj, pause, enables);
+endmodule
