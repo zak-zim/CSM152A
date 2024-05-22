1e40a5cad0fb8a31fded0a778c52346efab239d8
Marc Luzuriaga <Mjluzuriaga109@gmail.com>
2024-05-22 01:30:15 -0700

    Added constraints

--- lab3/StopTop.v
+++ lab3/StopTop.v
@@ -1,20 +1,20 @@
-module StopTop(reset, clk, sel, adj, pause);
+//module StopTop(reset, clk, sel, adj, pause);
 
-input reset;
-input clk;
-input sel;
-input adj;
-input pause;
+//input reset;
+//input clk;
+//input sel;
+//input adj;
+//input pause;
 
-reg counterClk;
-reg adjClk;
-reg dispClk;
-reg blinkClk;
-wire [2:0] enables;
-wire [5:0] min;
-wire [5:0] sec;
+//reg counterClk;
+//reg adjClk;
+//reg dispClk;
+//reg blinkClk;
+//wire [2:0] enables;
+//wire [5:0] min;
+//wire [5:0] sec;
 
-clock clock(clk, counterClk, adjClk, dispClk, blinkClk);
-StopController controller(clk, adj, pause, enables);
+//clock clock(clk, counterClk, adjClk, dispClk, blinkClk);
+//StopController controller(clk, adj, pause, enables);
 
-endmodule
+//endmodule
--- lab3/tb.v
+++ lab3/tb.v
@@ -1,23 +1,37 @@
 module tb;
 
-    reg clk;
-    wire outClk1;
-    wire outClk2;
-    wire outClk3;
-    wire outClk4;
-            
-    
-    clock UUT(clk, outClk1, outClk2, outClk3, outClk4);
-    integer i;
-    
-    initial begin
-        clk = 0;
-        #1000 $stop;
-    end
-    
+reg clk=0;
+reg[7:0] switch=0;
+reg[3:0] button;
+reg minutes = 3;
+reg seconds = 2;
+reg[3:0] AnodeSelect = 0;
+reg[6:0] LED_value = 0;
+seven_seg_display UUT(minutes, seconds, clk, Anode_select);
     always begin
         #5 clk = ~clk;
     end
+    
+    
+    
+//    reg clk;
+//    wire outClk1;
+//    wire outClk2;
+//    wire outClk3;
+//    wire outClk4;
+            
+    
+//    clock UUT(clk, outClk1, outClk2, outClk3, outClk4);
+//    integer i;
+    
+//    initial begin
+//        clk = 0;
+//        #1000 $stop;
+//    end
+    
+//    always begin
+//        #5 clk = ~clk;
+//    end
     
     
 endmodule
