4c9177fd51762d7974bedad8742ccc6958754d2c
Marc Luzuriaga <Mjluzuriaga109@gmail.com>
2024-05-29 15:48:53 -0700

    Implemented Clock Divider and Seven Segment Display

--- lab4/disp.v
+++ lab4/disp.v
@@ -1,4 +1,47 @@
-module disp(
-
-    );
+module disp(clk, p1_choice, p2_choice, leds, Anode);
+    
+    input clk;
+    input p1_choice;
+    input p2_choice;
+    output reg [6:0] leds;
+    output reg [3:0] Anode;
+    
+    reg [1:0] activate = 0;
+    reg [3:0] choice;
+    
+    initial begin
+        activate = 0;
+        choice = 4'b0000;
+    end
+    
+    always@ (posedge clk) begin
+        activate <= activate + 1;
+    end
+    
+    always@ (*) begin
+        case(activate)
+            2'b00: begin
+                Anode = 4'b0111;
+                choice  = p1_choice;
+            end
+            2'b01: begin
+                Anode = 4'b1011;
+                choice  = p2_choice;
+            end
+         endcase
+    end
+    
+    always@ (*) begin
+        case(choice)
+             //Rock - "r"
+             4'b0000: leds = 7'b1000110;
+             //Paper - "P"
+             4'b0001: leds = 7'b1100111; 
+             //Scissors - "S"
+             4'b0010: leds = 7'b1011011;
+         endcase
+    end
+    
 endmodule
+
+
--- lab4/tb.v
+++ lab4/tb.v
@@ -1,5 +1,20 @@
 module tb();
-
+    reg clk;
+    wire [6:0] hz = 10;
+    
+    clock UUT(clk, hz);
+    
+    integer  i = 0;
+    initial begin
+        clk = 0;
+        #1000 $stop;
+    end
+        
+        always begin
+            #5
+            clk = ~clk;
+            
+        end 
 
 
 endmodule
